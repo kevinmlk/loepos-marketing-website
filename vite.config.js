b649b487695d1a46b4087d605d7079094d880084
Kevin Malekera <r0900654@student.thomasmore.be>
2025-03-19 18:40:30 +0100

    feat: addes build lines in vite config to include the other pages when building for prod

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
         outDir: '../dist', // Output folder for build files (outside src)
         assetsDir: 'assets', // Output folder for assets
         emptyOutDir: true, // Clean output folder before building
+        polyfillDynamicImport: true, // Enable dynamic import polyfill
+        sourcemap: true, // Enable source maps
+        rollupOptions: {
+        input: {
+            main: 'src/index.html',
+            team: 'src/team.html', // Add this line for each page you want to build
+            prices: 'src/prijzen.html',
+            faq: 'src/faq.html',
+            contact: 'src/contact.html',
+        },
+        },
     },
 });
